2749d535e3632706dd4182dc58318eb65834295e
kevinCaris <kevinadossou2@gmail.com>
2023-08-11 11:01:49 +0100

    first commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,15 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello city</title>
+        <title>About-us | hello city</title>
 
 
     </head>
     <body >
-        <h1>Hello from Quebec !</h1>
-        <p>It's currently {{ date('h:i A')}}.</p>
+        <h1>Built with &hearts; by ADOSSOU kévin</h1>
+        <p><a href="/"> Recenir à la page d' accueil</a></p>
         <footer>
             <p>&copy; Copyright {{ date('Y')}} &middot; <a href="/about-us">About Us</a></p>
         </footer>
     </body>
 </html>
+ 
